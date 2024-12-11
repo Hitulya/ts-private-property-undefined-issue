f817e15d4b7f24e59ba8ec27f7efa2c462a7d289
Hitulya <Hitulya@users.noreply.github.com>
2024-12-11 06:29:53 +0300

    init

---
+++ src/Issue.ts
@@ -0,0 +1,4 @@
+export class Issue {
+  string2 = this.string1 + "b";
+  constructor(private readonly string1 = "d") {}
+}
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { Issue } from "./Issue";
+
+const _1 = new Issue();
+console.log(_1.string2);
